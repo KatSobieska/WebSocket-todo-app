e2b16e3b4531a9651296c81b56fc39a33f1da4e2
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-07-17 17:01:26 +0200

    Add event listeners for updateData, addTask and removeTask

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 const express = require("express");
 const socket = require("socket.io");
 
+let tasks = [];
+
 const app = express();
 
-const tasks = [];
-
-const server = app.listen(process.env.PORT || 8000, () => {
-  console.log("Server is running...");
+const server = app.listen(8000, () => {
+  console.log("Server is running on Port:", 8000);
 });
 
 app.use((req, res) => {
-  res.status(404).send({ message: "UPS.. page not found" });
+  res.status(404).send("404: Page not found");
 });
 
 const io = socket(server);
+
+io.on("connection", (socket) => {
+  console.log("Client id:" + socket.id);
+  socket.emit("updateData", tasks);
+  socket.on("addTask", (task) => {
+    console.log("Task added");
+    tasks.push(task);
+    socket.broadcast.emit("addTask", task);
+  });
+  socket.on("removeTask", () => {
+    console.log("Task removed");
+    const task = tasks.find((task) => task.id === socket.id);
+    tasks.splice(tasks.indexOf(task), 1);
+    socket.broadcast.emit("removeTask", task);
+  });
+});
