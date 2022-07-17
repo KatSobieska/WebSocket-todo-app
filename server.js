61260c5340c0e9de3ef3ba82b760e6b872255e8d
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-07-17 15:44:53 +0200

    Add socket.io and empty tasks arr

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const socket = require("socket.io");
 
 const app = express();
+
+const tasks = [];
 
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log("Server is running...");
@@ -9,3 +12,5 @@ const server = app.listen(process.env.PORT || 8000, () => {
 app.use((req, res) => {
   res.status(404).send({ message: "UPS.. page not found" });
 });
+
+const io = socket(server);
